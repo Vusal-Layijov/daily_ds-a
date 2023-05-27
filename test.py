16a429562726c2c6fc8236268c5873a2c61631fa
Vusal Layijov <Layijovvusal@gmail.com>
2023-05-27 12:14:25 -0700

    remove nth node from end of list

---
+++ test.py
@@ -0,0 +1,14 @@
+# //Remove Nth Node From End of List
+
+def removeN(head, n):
+    dummy = ListNode(0, head)
+    left = dummy
+    right = head
+    while n > 0 and right:
+        right = right.next
+        n -= 1
+    while right:
+        left = left.next
+        right = right.next
+    left.next = left.next.next
+    return dummy.next
