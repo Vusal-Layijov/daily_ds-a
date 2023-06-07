02ac4ed08751fd2224085dcef84cbd3eef0b175b
Vusal Layijov <Layijovvusal@gmail.com>
2023-06-07 16:54:43 -0700

    depth and breadth search traversal

--- binarysearch.js
+++ binarysearch.js
@@ -14,3 +14,36 @@ function binaryserach(arr,target){
     return false
 
 }
+// depth search
+function depthFirstTraversal(root){
+    if(root ===null) return 
+    const stack =[]
+    stack.push(root)
+    while(stack.length>0){
+        let node =stack.pop()
+        console.log(node.value)
+       if(node.right) stack.push(node.right)
+        if (node.left) stack.push(node.left)
+    }
+}
+
+function depthSearchRecursive(root){
+    if (root ===null) return []
+   
+    const left= depthSearchRecursive(root.left)
+    const right = depthSearchRecursive(root.right)
+ 
+    return [root.value,...left, ...right]
+}
+
+//breadth search traversal
+function breadthFirstTraversal(root){
+    const queue = []
+    queue.push(root)
+    while(queue.length>0){
+       let node=queue.shift()
+       console.log(node.value)
+       queue.push(node.left)
+       queue.push(node.right)
+    }
+}
