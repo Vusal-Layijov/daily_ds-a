2893cf1778ffc772bd47b7aeaac08ca3d671adfe
Vusal Layijov <Layijovvusal@gmail.com>
2023-07-27 17:00:22 -0700

    find dublicate

---
+++ js/findTheDublicate.js
@@ -0,0 +1,19 @@
+ function findDublicateNumber(nums){
+      let slow =0
+      let fast = 0
+      while (true){
+        slow = nums[slow]
+        fast=nums[nums[fast]]
+        if(slow===fast){
+            break
+        }
+      }
+      let slow2=0
+      while(true){
+        slow=nums[slow]
+        slow2=nums[slow2]
+        if(slow===slow2){
+            return slow
+        }
+      }
+ }
