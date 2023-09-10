f0d35b48f2d9c2724db52d95f2c7327ff8edb02d
Vusal Layijov <Layijovvusal@gmail.com>
2023-09-10 15:16:51 -0700

    is balanced

--- python/insertnode.py
+++ python/insertnode.py
@@ -27,3 +27,27 @@ def icecreamParlor(m, cost):
         flavor_indices[price] = i  # Store the current price's index
 
     return []
+
+
+def isBalanced(s):
+    stack = []  # Initialize an empty stack
+
+    # Define a dictionary to map closing brackets to their corresponding opening brackets
+    bracket_pairs = {')': '(', '}': '{', ']': '['}
+
+    # Iterate through each character in the string
+    for char in s:
+        if char in bracket_pairs.values():
+            # If it's an opening bracket, push it onto the stack
+            stack.append(char)
+        elif char in bracket_pairs.keys():
+            # If it's a closing bracket
+            if not stack:
+                return "NO"  # There's no matching opening bracket, so it's unbalanced
+            if stack[-1] == bracket_pairs[char]:
+                stack.pop()  # Pop the matching opening bracket from the stack
+            else:
+                return "NO"  # The brackets are not balanced
+
+    # After processing all characters, the stack should be empty if it's balanced
+    return "YES" if not stack else "NO"
