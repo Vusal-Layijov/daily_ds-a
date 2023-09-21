0797b400d32708b45e9adc9e9ac15a0dba6af081
Vusal Layijov <Layijovvusal@gmail.com>
2023-09-21 12:09:58 -0700

    intersect with cars

--- python/bigSorting.py
+++ python/bigSorting.py
@@ -3,3 +3,13 @@ def bigSorting(unsorted):
     unsorted.sort(key = lambda x: int(x))
 
     return unsorted
+
+
+def number_of_points(nums):
+    # Write your code here
+    myset = set()
+    for pair in nums:
+        for num in range(pair[0], pair[1]+1):
+            myset.add(num)
+    print(myset)
+    return len(myset)
