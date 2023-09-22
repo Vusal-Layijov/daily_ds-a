07bd7b1f3194315948cb909007759cf4d2c15ba1
Vusal Layijov <Layijovvusal@gmail.com>
2023-09-22 12:40:39 -0700

    mergeAlternately

--- python/bigSorting.py
+++ python/bigSorting.py
@@ -13,3 +13,18 @@ def number_of_points(nums):
             myset.add(num)
     print(myset)
     return len(myset)
+
+
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        togo = ''
+        count = 0
+        len1 = len(word1)
+        len2 = len(word2)
+        while count <= len1 or count <= len2:
+            if count < len1:
+                togo += word1[count]
+            if count < len2:
+                togo += word2[count]
+            count += 1
+        return togo
