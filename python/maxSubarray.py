fe3c8f2b065b378121db62ba0eaf6f95cbe773a6
Vusal Layijov <Layijovvusal@gmail.com>
2023-10-01 19:03:55 -0700

    can Place flowers

--- python/maxSubarray.py
+++ python/maxSubarray.py
@@ -47,3 +47,22 @@ def legoBlocks(n, m):
         total_ways = (total_ways + dp[height]) % MOD
 
     return total_ways
+
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        count = 0
+        i = 0
+
+        while i < len(flowerbed):
+            if flowerbed[i] == 0:
+                # Check if the previous and next plots are also empty or out of bounds
+                prev_empty = (i == 0 or flowerbed[i - 1] == 0)
+                next_empty = (i == len(flowerbed) - 1 or flowerbed[i + 1] == 0)
+
+                if prev_empty and next_empty:
+                    flowerbed[i] = 1  # Plant a flower
+                    count += 1
+            i += 1
+
+        return count >= n
