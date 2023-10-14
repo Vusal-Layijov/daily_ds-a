b050d234173bc804b66ba1fdd73009aa31e0dbbb
Vusal Layijov <Layijovvusal@gmail.com>
2023-10-14 13:19:55 -0700

    hackerLand2

--- js/hackerland.js
+++ js/hackerland.js
@@ -21,3 +21,32 @@ function hackerlandRadioTransmitters(x, k) {
 
 
 }
+function hackerlandRadioTransmitters(x, k) {
+    x.sort((a, b) => a - b); // Sort house locations in ascending order
+    const n = x.length;
+    let transmitters = 0;
+    let i = 0;
+
+    while (i < n) {
+        transmitters++; // Place a transmitter
+        let loc = x[i];
+
+        // Move to the rightmost house that can be covered
+        while (i < n && x[i] <= loc + k) {
+            i++;
+        }
+
+        loc = x[i - 1];
+
+        // Move to the right of the transmitter's range
+        while (i < n && x[i] <= loc + k) {
+            i++;
+        }
+    }
+
+    return transmitters;
+}
+
+// Example usage:
+
+
