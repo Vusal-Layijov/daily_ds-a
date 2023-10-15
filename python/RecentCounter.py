c14beadca0d267146a3254722db1cf322e03c8a1
Vusal Layijov <Layijovvusal@gmail.com>
2023-10-15 16:29:50 -0700

    lasStone

--- python/RecentCounter.py
+++ python/RecentCounter.py
@@ -8,3 +8,15 @@ class RecentCounter:
             self.requests.pop(0)
         self.requests.append(t)
         return len(self.requests)
+
+def last_stone(stones):
+    # Write your code here
+    while len(stones)>=2:
+        max1=max(stones)
+        stones.remove(max1)
+        max2=max(stones)
+        stones.remove(max2)
+        if max1==max2:
+            continue
+        stones.append(max1-max2)
+    return stones[0] if len(stones)>0 else 0
