6671917a6773e1d1d4cd56fc27e7edbc5badb923
Vusal Layijov <Layijovvusal@gmail.com>
2023-10-17 12:57:10 -0700

    isSubsequence

--- python/arrayManipulation.py
+++ python/arrayManipulation.py
@@ -15,3 +15,12 @@ def arrayManipulation(n, queries):
             max_value = current_value
 
     return max_value
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i=0
+        j=0
+        while i<len(s) and j<len(t):
+            if s[i] == t[j]:
+                i+=1
+            j+=1
+        return i==len(s)
