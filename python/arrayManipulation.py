2fe2784008f18b6ff88282f6ca12b521b2dda8d6
Vusal Layijov <Layijovvusal@gmail.com>
2023-10-17 19:00:15 -0700

    highestValuepalandrime

--- python/arrayManipulation.py
+++ python/arrayManipulation.py
@@ -24,3 +24,41 @@ class Solution:
                 i+=1
             j+=1
         return i==len(s)
+
+
+def highestValuePalindrome(s, n, k):
+    s = list(s)
+    left, right = 0, n - 1
+    num_changes = 0
+
+    while left < right:
+        if s[left] != s[right]:
+            max_char = max(s[left], s[right])
+            if max_char != '9' and num_changes < k - 1:
+                s[left] = '9'
+                s[right] = '9'
+                num_changes += 2
+            else:
+                s[left] = max_char
+                s[right] = max_char
+                num_changes += 1
+        left += 1
+        right -= 1
+
+    if n % 2 == 1 and num_changes < k:
+        s[n // 2] = '9'
+
+    if num_changes > k:
+        return "-1"
+
+    return ''.join(s)
+
+
+# Example usage:
+s1 = '1231'
+k1 = 3
+print(highestValuePalindrome(s1, len(s1), k1))  # Output: '9339'
+
+s2 = '12321'
+k2 = 1
+print(highestValuePalindrome(s2, len(s2), k2))  # Output: '12921'
