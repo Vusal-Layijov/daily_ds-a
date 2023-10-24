99a29ed640a75a87c3ed12066d0b114ac9bd37a2
Vusal Layijov <Layijovvusal@gmail.com>
2023-10-24 16:29:25 -0700

    longestOnes

--- python/maxvowels.py
+++ python/maxvowels.py
@@ -34,3 +34,20 @@ class Solution:
             maxK = max(maxK, count)
 
         return maxK
+
+
+class Solution:
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        zero_count = 0
+        left = 0
+        maxLen = 0
+        for right in range(len(nums)):
+            if nums[right] == 0:
+                zero_count += 1
+            while zero_count > k:
+                if nums[left] == 0:
+                    zero_count -= 1
+                left += 1
+
+            maxLen = max(maxLen, right-left+1)
+        return maxLen
