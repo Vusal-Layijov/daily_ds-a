a275bbd4ca19ada45f3a61513be0b2271a44a75f
Vusal Layijov <Layijovvusal@gmail.com>
2023-10-30 14:26:07 -0700

    decodeHuff

---
+++ python/decodeHuff.py
@@ -0,0 +1,18 @@
+def decodeHuff(root, s):
+
+    cur_node = root
+    encode = ""
+
+    for i in s:
+        if i == "0":
+            cur_node = cur_node.left
+        else:
+            cur_node = cur_node.right
+
+        if not cur_node.left and not cur_node.right:
+            encode += cur_node.data
+            cur_node = root
+        else:
+            cur_node = cur_node.right
+
+    print(encode)
