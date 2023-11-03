fe52be7c0fbe6e06cdd3c4f3c515a7fc2f8e5ae6
Vusal Layijov <Layijovvusal@gmail.com>
2023-11-03 11:42:37 -0700

    getMinSUm

--- python/test.py
+++ python/test.py
@@ -40,3 +40,34 @@ class Solution:
                 outputArr.append(chars)
 
         return ''.join(outputArr)
+
+
+def getMinSum(security_values, msg):
+    n = len(msg)
+    msg_values = [security_values[ord(ch) - ord('a')] for ch in msg]
+
+    # Sort the message values in ascending order
+    msg_values.sort()
+
+    # Initialize two pointers, one at the beginning and one at the end of the sorted values
+    left, right = 0, n - 1
+    min_sum = 0
+
+    while left < right:
+        # Calculate the absolute difference between adjacent characters
+        diff = msg_values[right] - msg_values[left]
+        min_sum += diff
+
+        # Move the pointers towards the center
+        left += 1
+        right -= 1
+
+    return min_sum
+
+
+# Example usage
+security_values = [1, 2, 1, 3, 1, 3, 5, 7, 1, 1, 5,
+                   5, 8, 10, 11, 1, 23, 2, 3, 7, 8, 9, 1, 6, 5, 9]
+message = "afeb"
+result = getMinSum(security_values, message)
+print(result)  # Output: 2
