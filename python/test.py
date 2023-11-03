4aca9ae66e35c3252ff7390314787e205c193a05
Vusal Layijov <Layijovvusal@gmail.com>
2023-11-02 19:45:42 -0700

    removeStars

--- python/test.py
+++ python/test.py
@@ -28,3 +28,15 @@ def copyrandom(head):
         copy.random = oldtoCopy[cur.random]
         cur = cur.next
     return oldtoCopy[head]
+
+
+class Solution:
+    def removeStars(self, s: str) -> str:
+        outputArr = []
+        for chars in s:
+            if chars == '*':
+                outputArr.pop()
+            else:
+                outputArr.append(chars)
+
+        return ''.join(outputArr)
