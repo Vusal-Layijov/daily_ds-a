22b48864075b6f9314bcf96a79395937ca5fd8f6
Vusal Layijov <Layijovvusal@gmail.com>
2023-11-03 15:48:52 -0700

    tribonacci

--- python/test.py
+++ python/test.py
@@ -71,3 +71,19 @@ security_values = [1, 2, 1, 3, 1, 3, 5, 7, 1, 1, 5,
 message = "afeb"
 result = getMinSum(security_values, message)
 print(result)  # Output: 2
+
+
+class Solution:
+    def tribonacci(self, n: int, memo={}) -> int:
+        if n in memo:
+            return memo[n]
+        if n <= 0:
+            return 0
+        elif n == 1:
+            return 1
+        elif n == 2:
+            return 1
+        else:
+            memo[n] = self.tribonacci(
+                n-1) + self.tribonacci(n-2) + self.tribonacci(n-3)
+            return memo[n]
