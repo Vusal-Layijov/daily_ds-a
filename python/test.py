0aa9e0a09284f96de7476721881e7e2044e613da
Vusal Layijov <Layijovvusal@gmail.com>
2023-11-06 22:14:27 -0800

    twoSum

--- python/test.py
+++ python/test.py
@@ -101,3 +101,21 @@ def findTotalCost(arr):
         arr.append(miN+maX)
         print(arr)
     return cost
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i = 0
+        j = len(numbers) - 1
+
+        while i < j:
+            s = numbers[i] + numbers[j]
+            if s == target:
+                return [i + 1, j + 1]
+
+            if s > target:
+                j -= 1
+            else:
+               i += 1
+
+        return []
