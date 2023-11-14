75a19b4651d165252259efa20490e4f1f7ba8fe6
Vusal Layijov <Layijovvusal@gmail.com>
2023-11-14 12:01:49 -0800

    houGlassSum

--- js/reverseWords.js
+++ js/reverseWords.js
@@ -6,3 +6,19 @@ var reverseWords = function (s) {
     console.log('ssss', son)
     return son.join(' ')
 };
+function hourglassSum(arr) {
+    // Write your code here
+    let maxS = -63
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            let nextS = arr[i].slice(j, j + 3).reduce((acc, cur) => acc + cur, 0)
+            nextS += arr[i + 1][j + 1]
+            nextS += arr[i + 2].slice(j, j + 3).reduce((acc, cur) => acc + cur, 0)
+            maxS = Math.max(maxS, nextS)
+        }
+
+    }
+    return maxS
+
+
+}
