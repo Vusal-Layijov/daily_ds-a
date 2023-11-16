60eeca3e7b42496c29f12dfe0c65ac718381900e
Vusal Layijov <Layijovvusal@gmail.com>
2023-11-15 22:55:31 -0800

    cheasarChipper

--- python/merchantCount.py
+++ python/merchantCount.py
@@ -13,3 +13,17 @@ def sockMerchant(n, ar):
         if num // 2 > 0:
             count += num // 2
     return count
+
+
+def caesarCipher(s, k):
+    ans = ""
+    for c in s:
+        if c.isalpha():
+            if c.islower():
+                ascii = ord('a') + (ord(c) - ord('a') + k) % 26
+            else:
+                ascii = ord('A') + (ord(c) - ord('A') + k) % 26
+            ans += chr(ascii)
+        else:
+            ans += c
+    return ans
