7c2fe3208a128152a5d18db78d6fd6a1378624ad
Vusal Layijov <Layijovvusal@gmail.com>
2023-11-23 15:21:52 -0800

    counntAppleAndOranges

--- python/getQueryAnswers.py
+++ python/getQueryAnswers.py
@@ -38,3 +38,17 @@ def staircase(n):
             
         print(''.join(myArr))
         n-=1
+    
+# countApplesAndOranges
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    # Write your code here
+    countApple = 0
+    countOrange = 0
+    for apple in apples:
+        if s <= a+apple <= t:
+            countApple += 1
+    for orange in oranges:
+        if s <= b+orange <= t:
+            countOrange += 1
+    print(countApple)
+    print(countOrange)
