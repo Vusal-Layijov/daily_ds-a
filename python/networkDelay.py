2ca02d19a8583171bd54cfde9b293de1891816cc
Vusal Layijov <Layijovvusal@gmail.com>
2023-11-29 14:03:35 -0800

    findChapestPrice

--- python/networkDelay.py
+++ python/networkDelay.py
@@ -57,3 +57,18 @@ def divisibleSumPairs(n, k, ar):
             if (ar[i]+ar[j]) % k == 0:
                 res.add((i, j))
     return len(res)
+
+
+class Solution:
+    def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        prices = [float('inf')]*n
+        prices[src] = 0
+        for i in range(k+1):
+            tmp = prices.copy()
+            for s, d, p in flights:
+                if prices[s] == float('inf'):
+                    continue
+                if prices[s]+p < tmp[d]:
+                    tmp[d] = prices[s]+p
+            prices = tmp
+        return -1 if prices[dst] == float('inf') else prices[dst]
