8be13a44887ad429ebadce6a194adec24e85e671
Vusal Layijov <Layijovvusal@gmail.com>
2023-12-10 15:30:17 -0800

    rottenOrange

--- python/arrayManipulation.py
+++ python/arrayManipulation.py
@@ -62,3 +62,29 @@ print(highestValuePalindrome(s1, len(s1), k1))  # Output: '9339'
 s2 = '12321'
 k2 = 1
 print(highestValuePalindrome(s2, len(s2), k2))  # Output: '12921'
+
+
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        q=[]
+        time,fresh=0,0
+        rows,cols=len(grid),len(grid[0])
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c]==1:
+                    fresh+=1
+                if grid[r][c]==2:
+                    q.append([r,c])
+        directions=[[0,1],[0,-1],[1,0],[-1,0]]
+        while q and fresh>0:
+            for i in range(len(q)):
+                r,c=q.pop(0)
+                for dr,dc in directions:
+                    row,col=r+dr,c+dc
+                    if(row<0 or row==len(grid) or col<0 or col==len(grid[0]) or grid[row][col]!=1):
+                        continue
+                    grid[row][col]=2
+                    fresh-=1
+                    q.append([row,col])
+            time+=1
+        return time if fresh==0 else -1
