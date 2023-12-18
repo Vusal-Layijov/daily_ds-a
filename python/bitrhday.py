792226023ae435a3efb5143742800cf77ed9e1e2
Vusal Layijov <Layijovvusal@gmail.com>
2023-12-18 15:33:59 -0800

    bfs

--- python/bitrhday.py
+++ python/bitrhday.py
@@ -21,3 +21,28 @@ def pickingNumbers(a):
         if previousSet > finalSet:
             finalSet = previousSet
     return finalSet
+#bfs 
+
+def bfs(n,m,edges,s):
+    vis=[False for i in range(n+1)]
+    dis = {i:-1 for i in range(n+1)}
+    adj=[[] for i in range(n+1)]
+    for item in edges:
+        adj[item[0]].append(item[1])
+        adj[item[1]].append(item[0])
+    dis[s]=0
+    vis[s]=True
+    q=[s]
+    while q:
+        cur=q.pop(0)
+        for nei in adj[cur]:
+            if not vis[nei]:
+                dis[nei]=6+dis[cur]
+                q.append(nei)
+                vis[nei]=True
+
+    del dis[0]
+    del dis[s]
+    return list(dis.values())
+
+            
