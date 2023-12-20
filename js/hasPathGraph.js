cef80e6e3d46e1ba9ce17f3f1cba231866f1809c
Vusal Layijov <Layijovvusal@gmail.com>
2023-12-20 11:49:40 -0800

    bfs2

--- js/hasPathGraph.js
+++ js/hasPathGraph.js
@@ -19,3 +19,33 @@ const hasPathBreadth = (graph, src, dst) =>{
     }
     return false
 }
+function bfs(n, m, edges, s) {
+    const vis = new Array(n + 1).fill(false);
+    const dis = {};
+    const adj = new Array(n + 1).fill(null).map(() => []);
+
+    edges.forEach((item) => {
+        adj[item[0]].push(item[1]);
+        adj[item[1]].push(item[0]);
+    });
+
+    const qu = [s];
+    vis[s] = true;
+    dis[s] = 0;
+
+    while (qu.length > 0) {
+        const ele = qu.shift();
+        for (const nei of adj[ele]) {
+            if (!vis[nei]) {
+                vis[nei] = true;
+                dis[nei] = 6 + dis[ele];
+                qu.push(nei);
+            }
+        }
+    }
+
+    delete dis[0];
+    delete dis[s];
+
+    return Object.values(dis);
+}
