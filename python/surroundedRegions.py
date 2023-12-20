5df1db65e2dd51a20dd2aff7443ae5d92ca87c8c
Vusal Layijov <Layijovvusal@gmail.com>
2023-12-20 15:34:55 -0800

    construct the array

--- python/surroundedRegions.py
+++ python/surroundedRegions.py
@@ -26,5 +26,20 @@ def solve(board):
                 board[r][c] = 'o'
 
 
+def countArray(n, k, x):
+    # Return the number of ways to fill in the array.
+    a = [0] * n
+    b = [0] * n
 
+    if x != 1:
+        a[0] = 0
+        b[0] = 1
+    else:
+        a[0] = 1
+        b[0] = 0
 
+    for i in range(1, n):
+        a[i] = b[i - 1]
+        b[i] = (a[i - 1] * (k - 1) + b[i - 1] * (k - 2)) % (10**9 + 7)
+
+    return a[n - 1]
