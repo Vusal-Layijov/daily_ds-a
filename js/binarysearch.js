52bd1b044dde568e3600e706bc06b228e09eef9b
Vusal Layijov <Layijovvusal@gmail.com>
2023-12-21 12:30:28 -0800

    mergeTwoLists

--- js/binarysearch.js
+++ js/binarysearch.js
@@ -121,3 +121,26 @@ function preOrder(root) {
     const out = preOrderRec(root);
     console.log(out);
 }
+var mergeTwoLists = function (list1, list2) {
+    let dum = new ListNode()
+    let tail = dum
+    while (list1 && list2) {
+        if (list1.val <= list2.val) {
+            tail.next = list1
+            list1 = list1.next
+
+        } else {
+            tail.next = list2
+            list2 = list2.next
+        }
+        tail = tail.next
+    }
+    if (list1) {
+        tail.next - list1
+    }
+    if (list2) {
+        tail.next = list2
+    }
+    return dum.next
+
+};
