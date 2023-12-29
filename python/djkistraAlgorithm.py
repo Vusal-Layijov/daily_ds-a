fe43a0052c303ab5f42146da6299e673b9b82677
Vusal Layijov <Layijovvusal@gmail.com>
2023-12-29 14:49:19 -0800

    djkistra algorithmO

---
+++ python/djkistraAlgorithm.py
@@ -0,0 +1,29 @@
+from heapq import heappush, heappop
+
+def shortestReach(n, edges, s):
+    
+    g = [set() for _ in range(n+1)]
+    for u, v, w in edges:
+        g[u].add((v, w))
+        g[v].add((u, w))
+
+    queue = [(0, s)]
+    bag = {s: 0}
+    processed = [-1] * (n+1)
+    
+    while queue:
+        
+        cost, u = heappop(queue)
+        
+        if processed[u] != -1: continue
+        processed[u] = cost
+        
+        for v, w in g[u]:
+            if processed[v] == -1:
+                curr_cost = bag.get(v, float("inf"))
+                new_cost = cost + w
+                if curr_cost > new_cost:
+                    heappush(queue, (new_cost, v))
+                    bag[v] = new_cost
+    
+    return processed[1:s] + processed[s+1:]
