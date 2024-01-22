ba17644445aba538ad4ac852c18b41f4583c62e7
Vusal Layijov <Layijovvusal@gmail.com>
2024-01-21 17:44:29 -0800

    subsets

--- python/waiter.py
+++ python/waiter.py
@@ -101,3 +101,18 @@ class Solution:
             count=brep.count('1')
             res.append(count)
         return res
+    
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res=[]
+        subset=[]
+        def dfs(i):
+            if i>=len(nums):
+                res.append(subset)
+                return
+            subset.append(nums[i])
+            dfs(i+1)
+            subset.pop()
+            dfs(i+1)
+        dfs(0)
+        return res
