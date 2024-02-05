10f972d92997c1ebb00ab64bcd56c16ae1b6234f
Vusal Layijov <Layijovvusal@gmail.com>
2024-02-05 13:44:54 -0800

    countPaths

---
+++ python/countPaths.py
@@ -0,0 +1,17 @@
+def count_paths(grid):
+  return _count_paths(grid,0,0,{})
+  
+
+  
+def _count_paths(grid,r,c,memo):
+  pos=(r,c)
+  if pos in memo:
+    return memo[pos]
+  if r==len(grid) or c==len(grid[0]) or grid[r][c] == 'X':
+    return 0
+  if r==len(grid)-1 and c==len(grid[0])-1:
+    return 1
+  dc= _count_paths(grid,r+1,c,memo)
+  rc=_count_paths(grid,r,c+1,memo)
+  memo[pos]=dc+rc
+  return memo[pos]
