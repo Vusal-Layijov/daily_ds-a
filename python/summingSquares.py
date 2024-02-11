222c69a8a272364c4d84ca0be7a9addcdb9c5706
Vusal Layijov <Layijovvusal@gmail.com>
2024-02-11 14:19:27 -0800

    arrayStepper

--- python/summingSquares.py
+++ python/summingSquares.py
@@ -66,3 +66,21 @@ def acmTeam(topic):
     res2.append(num)
     res2.append(res.count(num))
     return res2
+
+#array stepper
+def array_stepper(numbers):
+  return _array_stepper(numbers,0,{})
+
+
+def _array_stepper(numbers,i,memo):
+  if i in memo:
+    return memo[i]
+  if i>=len(numbers)-1:
+    return True
+  maxStep=numbers[i]
+  for step in range(1,maxStep+1):
+    if _array_stepper(numbers,i+step,memo):
+      memo[i]=True
+      return True
+  memo[i]=False
+  return memo[i]
