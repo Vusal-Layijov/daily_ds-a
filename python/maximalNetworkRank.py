f16ca79d17fb181314aad23a0c02ba2c53910dfb
Vusal Layijov <Layijovvusal@gmail.com>
2024-02-18 13:57:00 -0800

    minFuelCost

--- python/maximalNetworkRank.py
+++ python/maximalNetworkRank.py
@@ -50,3 +50,23 @@ def beautifulTriplets(d, arr):
         if num+d in arr and num + 2*d in arr:
             count+=1
     return count
+
+class Solution:
+    def minimumFuelCost(self, roads: List[List[int]], seats: int) -> int:
+        adjacency_list = defaultdict(list)
+        for a, b in roads:
+            adjacency_list[a].append(b)
+            adjacency_list[b].append(a)            
+        print(dict(adjacency_list))
+        total_fuel_cost = [0]        
+        def dfs(node, parent):
+            people = 1            
+            for neighbor in adjacency_list[node]:
+                if neighbor == parent:
+                    continue
+                people += dfs(neighbor, node)                
+            if node != 0:
+                total_fuel_cost[0] += math.ceil(people / seats)                
+            return people        
+        dfs(0, None)
+        return total_fuel_cost[0]
