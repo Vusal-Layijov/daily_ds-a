8a736773fbba15861ec37440003ae28a086a1920
Vusal Layijov <Layijovvusal@gmail.com>
2024-02-28 16:51:02 -0800

    reacheableNodes

--- python/maxroottoleafpathsum.py
+++ python/maxroottoleafpathsum.py
@@ -5,3 +5,12 @@ def maxRootToLeafPathSum(root):
         return root.val
     max_child_path_sum = max(maxRootToLeafPathSum(root.left, root.right))
     return root.val + max_child_path_sum
+
+class Solution:
+    def reachableNodes(self, n: int, edges: List[List[int]], restricted: List[int]) -> int:
+        myset=set([0])
+        for a,b in edges:
+            if ((a==0 or b==0 or a in myset or b in myset) and (a not in restricted and b not in restricted)):
+                myset.add(a)
+                myset.add(b)
+        return len(myset)
