ed30ee0105989072171f357e7443f3078c304b8e
Vusal Layijov <Layijovvusal@gmail.com>
2024-02-29 11:20:42 -0800

    reacheableNodes2

--- python/maxroottoleafpathsum.py
+++ python/maxroottoleafpathsum.py
@@ -14,3 +14,32 @@ class Solution:
                 myset.add(a)
                 myset.add(b)
         return len(myset)
+    
+class Solution:
+    def reachableNodes(self, n: int, edges: List[List[int]], restricted: List[int]) -> int:
+
+        #building a undirected graph
+        graph=collections.defaultdict(list)
+        def buildgraph(edge):
+            for x in edge:
+                graph[x[0]].append(x[1])
+                graph[x[1]].append(x[0])
+        restricted=set(restricted)
+        buildgraph(edges)
+
+        #breadth first search of a graph
+        count=0
+        q=[]
+        q.append(0)
+        visited=[True]*n
+        while q:
+            cur=q[0]
+            q.remove(cur)
+            #if not visited,taken into account
+            if visited[cur]:
+                count+=1
+                visited[cur]=False
+            for x in graph[cur]:
+                if visited[x] and x not in restricted :
+                    q.append(x)
+        return count
