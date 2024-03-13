cf5965681fa27bb522f8c9371b072f0cd70f94e5
Vusal Layijov <Layijovvusal@gmail.com>
2024-03-13 13:53:46 -0700

    gardenNoAdj

--- python/kingdomPath.py
+++ python/kingdomPath.py
@@ -122,3 +122,25 @@ def strangeCounter(t):
     if count==t: return 1
     else:
         return base-(t-count-1)
+#gardenNoAdj
+class Solution:
+    def gardenNoAdj(self, n: int, paths: List[List[int]]) -> List[int]:
+        lst=[[0]*4 for i in range(n)]
+        visited=[0]*(n+1)
+        adj=[[] for i in range(n+1)]
+        for i,j in paths:
+            adj[i].append(j)
+            adj[j].append(i)
+        visited[0]=1
+        ans=[0]*n
+        for i in range(1,n+1):
+            st=[i]
+            while st:
+                x=st.pop(0)
+                ans[x-1]=lst[x-1].index(0)+1
+                for t in adj[x]:
+                    if visited[t]==0:
+                        st.append(t)
+                        visited[t]=1
+                    lst[t-1][ans[x-1]-1]=1
+        return ans
