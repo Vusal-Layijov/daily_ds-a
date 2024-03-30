dce20e186e7df5b1f43370f0f3c7979ee17482a7
Vusal Layijov <Layijovvusal@gmail.com>
2024-03-30 13:38:48 -0700

    seperateNumbers

--- python/dinamicArray.py
+++ python/dinamicArray.py
@@ -55,3 +55,22 @@ def removeNthFromEnd(head, n):
         r-=1
     dummy.next=nextNode
     return head
+
+
+#separateNumbers
+def separateNumbers(s):
+    if s[0] == '0' or len(s) == 1:
+        print('NO')
+        return
+    
+    for i in range(1, len(s) // 2 + 1):
+        x = int(s[:i])
+        beautiful_number = str(x)
+        while len(beautiful_number) < len(s):
+            x += 1
+            beautiful_number += str(x)
+        if beautiful_number == s:
+            print('YES', s[:i])
+            return
+    
+    print('NO')
