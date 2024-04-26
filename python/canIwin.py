7861537e775fad93f2506e1ef4ed28e6590189a1
Vusal Layijov <Layijovvusal@gmail.com>
2024-04-26 13:58:06 -0700

    isanagram

--- python/canIwin.py
+++ python/canIwin.py
@@ -112,3 +112,11 @@ print(get_all_subarrays(array))
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         return len(set(nums))!=len(nums)
+    
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        l1 = list(s)
+        l2=list(t)
+        l1.sort()
+        l2.sort()
+        return l1==l2
