eb52cefa79d1abd07b5bbadc4203ccc285b5e30e
Vusal Layijov <Layijovvusal@gmail.com>
2024-04-28 14:43:53 -0700

    groupAnagrams

--- python/canIwin.py
+++ python/canIwin.py
@@ -136,3 +136,14 @@ class Solution:
                 max_profit = price - min_price  # Calculate max profit from the lowest price point
         
         return max_profit
+    
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = defaultdict(list)
+
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
