5ac037f206f0ee1ed89197167bc8a5788676935c
Vusal Layijov <Layijovvusal@gmail.com>
2024-04-30 17:35:56 -0700

    twoSum

--- python/canIwin.py
+++ python/canIwin.py
@@ -147,3 +147,20 @@ class Solution:
                 count[ord(c) - ord("a")] += 1
             ans[tuple(count)].append(s)
         return ans.values()
+    
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        i = 0
+        j = len(numbers) -1
+        
+        while i<j:
+            s = numbers[i] + numbers[j]
+            if s == target:
+                return [i + 1 , j + 1]
+            
+            if s > target:
+                j-=1
+            else:
+               i+=1 
+        
+        return []
