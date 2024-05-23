a803fa418ffb9ee73f3549df83189d2d0faec62a
Vusal Layijov <Layijovvusal@gmail.com>
2024-05-23 13:43:27 -0700

    rottenOranges2

--- js/minimumMoves.js
+++ js/minimumMoves.js
@@ -106,3 +106,48 @@ var longestPalindrome = function (s) {
     }
     return res
 };
+function orangesRotting(grid) {
+    let q = [];
+    let fresh = 0;
+    let time = 0;
+
+    // Initialize the queue with all rotten oranges and count fresh oranges
+    for (let r = 0; r < grid.length; r++) {
+        for (let c = 0; c < grid[0].length; c++) {
+            if (grid[r][c] === 1) {
+                fresh += 1;
+            }
+            if (grid[r][c] === 2) {
+                q.push([r, c]);
+            }
+        }
+    }
+
+    const directions = [[0, 1], [0, -1], [1, 0], [-1, 0]];
+
+    while (fresh > 0 && q.length > 0) {
+        let length = q.length;
+        for (let i = 0; i < length; i++) {
+            let [r, c] = q.shift();
+
+            for (let [dr, dc] of directions) {
+                let row = r + dr;
+                let col = c + dc;
+
+                // If in bounds and fresh orange, make it rotten and add to queue
+                if (
+                    row >= 0 && row < grid.length &&
+                    col >= 0 && col < grid[0].length &&
+                    grid[row][col] === 1
+                ) {
+                    grid[row][col] = 2;
+                    q.push([row, col]);
+                    fresh -= 1;
+                }
+            }
+        }
+        time += 1;
+    }
+
+    return fresh === 0 ? time : -1;
+}
