114999b44f23add52009ab2c02aa6bdc98dbd774
Vusal Layijov <Layijovvusal@gmail.com>
2024-05-29 14:22:47 -0700

    expectSelfProduct

--- python/arrayManipulation.py
+++ python/arrayManipulation.py
@@ -121,4 +121,16 @@ def _sum_possible(amount, numbers,memo):
       memo[amount]=True
       return True
   memo[amount]=False
-  return False  
+  return False 
+
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        res = [1] * (len(nums))
+
+        for i in range(1, len(nums)):
+            res[i] = res[i-1] * nums[i-1]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
