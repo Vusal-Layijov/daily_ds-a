edf41cb8256390e90273e1cd8e1107e0ff60e111
Vusal Layijov <Layijovvusal@gmail.com>
2024-06-11 16:08:10 -0700

    validSudoku

--- python/coinChange.py
+++ python/coinChange.py
@@ -6,3 +6,25 @@ def coinChange(coins,amount):
             if a-c>=0:
                 dp[a]= min(dp[a], 1+ dp[a-c])
     return dp[amount] if dp[amount] != amount +1 else -1 
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)  # key = (r /3, c /3)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (
+                    board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]
+                ):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
+        return True
