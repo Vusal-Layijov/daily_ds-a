732f525889cbab388de2d95943d86b93c6ed9140
Vusal Layijov <Layijovvusal@gmail.com>
2024-07-07 17:17:06 -0700

    dailyTemperatures2

--- python/isPowerOfTwo.py
+++ python/isPowerOfTwo.py
@@ -153,3 +153,14 @@ class Solution:
                     ans[i]=j
                     break
         return ans
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res=[0]*len(temperatures)
+        stack=[]
+        for i,t in enumerate(temperatures):
+            while stack and t>stack[-1][0]:
+                stackT,stackInd=stack.pop()
+                res[stackInd]=i-stackInd
+            stack.append([t,i])
+
+        return res
