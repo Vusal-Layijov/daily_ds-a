fdaad3bcfaedd22bf7ba3b12eeb79c14c2a2d620
Vusal Layijov <Layijovvusal@gmail.com>
2024-07-20 21:45:24 -0700

    longestConsecutive2

--- python/gemStones.py
+++ python/gemStones.py
@@ -13,3 +13,21 @@ def gemstones(arr):
         if realG == length:
             count += 1
     return count
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+
+        for n in nums:
+            if n-1 in numSet:
+                continue
+                
+            currentSequenceSize = 1
+
+            while (n+currentSequenceSize) in numSet:
+                currentSequenceSize += 1
+
+            longest = max(currentSequenceSize, longest)
+    
+        return longest
