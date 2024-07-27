207f79de9203b43d1d31f35990caa5a5154a4e3d
Vusal Layijov <Layijovvusal@gmail.com>
2024-07-27 12:58:01 -0700

    largestPermutation

--- js/minUnfairness.js
+++ js/minUnfairness.js
@@ -7,3 +7,37 @@ function minUnFairness(k,arr){
     }
     return minU
 }
+function largestPermutation(k, arr) {
+    const n = arr.length;
+    const pos = new Map();
+
+    // Create a map to store the position of each element
+    for (let i = 0; i < n; i++) {
+        pos.set(arr[i], i);
+    }
+
+    for (let i = 0; i < n && k > 0; i++) {
+        // The value that should be here if the array was sorted in descending order
+        const valueToPlace = n - i;
+
+        // If the value is already in the correct place, continue
+        if (arr[i] === valueToPlace) {
+            continue;
+        }
+
+        // Find the position of the valueToPlace
+        const posToSwap = pos.get(valueToPlace);
+
+        // Swap the values in the array
+        [arr[i], arr[posToSwap]] = [arr[posToSwap], arr[i]];
+
+        // Update the positions in the map
+        pos.set(arr[posToSwap], posToSwap);
+        pos.set(arr[i], i);
+
+        // Decrement k as we used one swap
+        k--;
+    }
+
+    return arr;
+}
