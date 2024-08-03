d5e6876c9c9aa3bb4bc64ee2f4fc79310088c21a
Vusal Layijov <Layijovvusal@gmail.com>
2024-08-02 18:23:32 -0700

    beutifulPairs2

--- js/cookies.js
+++ js/cookies.js
@@ -47,3 +47,30 @@ function dayOfProgrammer(year) {
     }
     return d + '.' + m + '.' + year
 }
+function beautifulPairs(A, B) {
+    let total = 0;
+
+    if (A.length === 1) {
+        return total;
+    }
+
+    for (let i = 0; i < A.length; i++) {
+        let a = A[i];
+        let index = B.indexOf(a);
+        if (index !== -1) {
+            total += 1;
+            B.splice(index, 1);
+        }
+    }
+
+    if (total < A.length) {
+        return total + 1;
+    } else {
+        return A.length - 1;
+    }
+}
+
+// Example usage:
+let A = [1, 2, 3, 4];
+let B = [1, 2, 3, 3];
+console.log(beautifulPairs(A, B)); // Output: 4
