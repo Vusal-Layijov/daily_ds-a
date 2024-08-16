6d7b0f9d3b110dba4e31be5a4829993d3e3b6fe1
Vusal Layijov <Layijovvusal@gmail.com>
2024-08-16 14:38:17 -0700

    postOrder

--- python/cloneGraph.py
+++ python/cloneGraph.py
@@ -37,4 +37,12 @@ def preOrder(root):
         preOrder(root.left)
     if root.right:
         preOrder(root.right)
-    
+def postOrder(root):
+    #Write your code here
+    if not root:
+        return
+    if root.left:
+        postOrder(root.left)
+    if root.right:
+        postOrder(root.right)
+    print(root.info, end=' ')
