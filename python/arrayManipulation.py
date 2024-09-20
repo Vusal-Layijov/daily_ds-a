0b4baf4e20a659d51e249037ba53b1104e8ba7a1
Vusal Layijov <Layijovvusal@gmail.com>
2024-09-20 11:20:35 -0700

    removeELement

--- python/arrayManipulation.py
+++ python/arrayManipulation.py
@@ -134,3 +134,11 @@ class Solution:
             res[i] *= postfix
             postfix *= nums[i]
         return res
+class Solution(object):
+    def removeElement(self, nums, val):
+        i = 0
+        for x in nums:
+            if x != val:
+                nums[i] = x
+                i += 1
+        return i
