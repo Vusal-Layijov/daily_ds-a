2767c090261744e1af16526764ebcfaf827b4cc7
Vusal Layijov <Layijovvusal@gmail.com>
2024-09-30 13:34:54 -0700

    indexOfMinimum

--- js/test2.mjs
+++ js/test2.mjs
@@ -39,3 +39,21 @@ function countPizzaOccurrences(data) {
     return count;
 }
 console.log(getTopicCount('pizza'))
+function solution(a) {
+
+    var indexOfMinimum = -1;
+    var minimalSum = Number.MAX_VALUE;
+
+    for (var i = 0; i < a.length; i++) {
+        var sum = 0;
+        for (var j = 0; j < a.length; j++) {
+            sum += Math.abs(a[i] - a[j]);
+        }
+        if (sum < minimalSum) {
+            minimalSum = sum;
+            indexOfMinimum = i;
+        }
+    }
+
+    return a[indexOfMinimum];
+}
