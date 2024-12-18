0bba03023a20df60610a67ea2999ba39b33e303f
Vusal Layijov <Layijovvusal@gmail.com>
2024-12-17 16:49:16 -0800

    Num Decoding buttom up

--- python/course.py
+++ python/course.py
@@ -374,3 +374,16 @@ class Solution:
             return res
 
         return dfs(0)
+    
+# Buttom up
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = {len(s) : 1}
+        for i in range(len(s)-1,-1,-1):
+            if s[i]=='0':
+                dp[i]=0
+            else:
+                dp[i]=dp[i+1]
+            if i<len(s)-1 and (s[i]=="1" or (s[i]=='2' and s[i+1] in "0123456") ) :
+                dp[i]+=dp[i+2]
+        return dp[0]
