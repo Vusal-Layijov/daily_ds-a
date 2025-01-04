dfe86738618474f5f618d640c8fd5d3f08c7b38c
Vusal Layijov <Layijovvusal@gmail.com>
2025-01-03 16:05:27 -0800

    minDifference

--- js/test2.mjs
+++ js/test2.mjs
@@ -79,3 +79,15 @@ var maxProduct = function (nums) {
     }
     return res;
 };
+var minimumDifference = function (nums, k) {
+    if (nums.length == 1) {
+        return 0
+    }
+    nums.sort((a, b) => a - b)
+    let minS = Infinity
+    for (let i = 0; i <= nums.length - k; i++) {
+        minS = Math.min(minS, nums[i + k - 1] - nums[i])
+    }
+    return minS
+
+};
