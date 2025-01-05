a1ceefd05782b78702194dcd32b2438fa546b769
Vusal Layijov <Layijovvusal@gmail.com>
2025-01-04 18:36:08 -0800

    mergeAlternately

--- js/test2.mjs
+++ js/test2.mjs
@@ -91,3 +91,16 @@ var minimumDifference = function (nums, k) {
     return minS
 
 };
+var mergeAlternately = function (word1, word2) {
+    let res = '';
+    let i = 0;
+
+    // Interleave characters from both strings
+    while (i < word1.length || i < word2.length) {
+        if (i < word1.length) res += word1[i];
+        if (i < word2.length) res += word2[i];
+        i++;
+    }
+
+    return res;
+};
