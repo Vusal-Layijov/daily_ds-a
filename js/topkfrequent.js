e63fedaccb4fa175f390a327bdbc15b195b93d3d
Vusal Layijov <Layijovvusal@gmail.com>
2025-01-27 11:15:40 -0800

    searchInsert

--- js/topkfrequent.js
+++ js/topkfrequent.js
@@ -46,3 +46,19 @@ var searchInsert = function (nums, target) {
     }
     return start
 };
+var guessNumber = function (n) {
+    let low = 1;
+    let high = n;
+
+    while (low <= high) {
+        let mid = Math.floor((low + high) / 2);
+        let res = guess(mid);
+        if (res === 0) {
+            return mid;
+        } else if (res === 1) {
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+};
