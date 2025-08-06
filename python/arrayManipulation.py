0d372eb5527e533d5f8a56820905d8cc92419cc6
Vusal Layijov <Layijovvusal@gmail.com>
2025-08-05 22:48:55 -0700

    reclor

--- python/arrayManipulation.py
+++ python/arrayManipulation.py
@@ -142,3 +142,17 @@ class Solution(object):
                 nums[i] = x
                 i += 1
         return i
+class Solution:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+        l=0
+        recolor=0
+        res=k
+        for r in range(len(blocks)):
+            if blocks[r]=='W':
+                recolor+=1
+            if r-l+1==k:
+                res=min(res,recolor)
+                if blocks[l]=='W':
+                    recolor-=1
+                l+=1
+        return res
